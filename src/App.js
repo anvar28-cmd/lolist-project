19af351eb57181f7fa74f1f9403240a041403cd8
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-03-10 05:34:48 -0500

    font format change

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
-import { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AppRoute } from './const';
-import Builds from './pages/Builds/Builds';
-import Heroes from './pages/Heroes/Heroes';
-import HeroesSelected from './pages/HeroesSelected/HeroesSelected';
-import Home from './pages/Home/Home';
-import Layout from './pages/Layout/Layout';
-import NotFound from './pages/NotFound/NotFound';
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-import './styles/style.scss';
-import { getToken } from './token';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppRoute } from "./const";
+import Builds from "./pages/Builds/Builds";
+import Heroes from "./pages/Heroes/Heroes";
+import HeroesSelected from "./pages/HeroesSelected/HeroesSelected";
+import Home from "./pages/Home/Home";
+import Layout from "./pages/Layout/Layout";
+import NotFound from "./pages/NotFound/NotFound";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import "./styles/style.scss";
+import { getToken } from "./token";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [isAuthorized, setIsAuthorized] = useState(getToken());
 
   return (
-    
     <BrowserRouter>
-     <ToastContainer 
-  position="top-center"
-  autoClose={5000}
-  hideProgressBar={false}
-  newestOnTop={false}
-  closeOnClick
-  rtl={false}
-  pauseOnFocusLoss
-  draggable
-  pauseOnHover
-  theme="light"/>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Routes>
-        <Route 
-          path={AppRoute.LOGIN} 
+        <Route
+          path={AppRoute.LOGIN}
           element={
-            <Login 
-              isAuthorized={isAuthorized} 
-              setIsAuthorized={setIsAuthorized} 
+            <Login
+              isAuthorized={isAuthorized}
+              setIsAuthorized={setIsAuthorized}
             />
           }
         />
 
-        <Route 
-          path={AppRoute.SIGN_UP} 
+        <Route
+          path={AppRoute.SIGN_UP}
           element={
-            <SignUp 
-              isAuthorized={isAuthorized} 
-              setIsAuthorized={setIsAuthorized} 
+            <SignUp
+              isAuthorized={isAuthorized}
+              setIsAuthorized={setIsAuthorized}
             />
           }
         />
-        
-        <Route 
-          path={AppRoute.HOME} 
+
+        <Route
+          path={AppRoute.HOME}
           element={
-            <Layout 
-              isAuthorized={isAuthorized} 
-              setIsAuthorized={setIsAuthorized} 
+            <Layout
+              isAuthorized={isAuthorized}
+              setIsAuthorized={setIsAuthorized}
             />
           }
         >
@@ -65,14 +65,28 @@ function App() {
 
           <Route path={AppRoute.HEROES} element={<Heroes />} />
 
-          <Route path={AppRoute.HEROES_SELECTED} element={<HeroesSelected isAuthorized={isAuthorized} 
-              setIsAuthorized={setIsAuthorized} />} />
+          <Route
+            path={AppRoute.HEROES_SELECTED}
+            element={
+              <HeroesSelected
+                isAuthorized={isAuthorized}
+                setIsAuthorized={setIsAuthorized}
+              />
+            }
+          />
 
-          <Route path={AppRoute.BUILDS} element={<Builds isAuthorized={isAuthorized} 
-              setIsAuthorized={setIsAuthorized} />} />
+          <Route
+            path={AppRoute.BUILDS}
+            element={
+              <Builds
+                isAuthorized={isAuthorized}
+                setIsAuthorized={setIsAuthorized}
+              />
+            }
+          />
         </Route>
 
-        <Route path={AppRoute.NOT_FOUND} element={<NotFound />}/>
+        <Route path={AppRoute.NOT_FOUND} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
